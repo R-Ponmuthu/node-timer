6fe47a0c7c3950bd6fc7ca03d74cca8b234d53fe
R-Ponmuthu <42769307+R-Ponmuthu@users.noreply.github.com>
2021-12-28 08:43:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const countdown = 181000;
+
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+setInterval(function() {
+  countdown=countdown-1000;
+  io.emit('timer', { countdown: countdown });
+  if(countdown == 0){
+    countdown = 181000
+  }
+}, 1000);
+
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
